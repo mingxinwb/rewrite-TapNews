9102f542795f9a00740ff981d4647954e76561d5
mingxinwb <mingxinwb@gmail.com>
2018-07-14 02:21:16 -0700

    generate server

---
+++ server/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var createError = require('http-errors');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, '../client/build/'));
+app.set('view engine', 'jade');
+
+app.use('/static',
+        express.static(path.join(__dirname, '../client/build/static/')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+module.exports = app;
